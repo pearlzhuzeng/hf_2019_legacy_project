30ff9695cbb0e798a65b7b276c60fad8b3ef864b
Pearl Zeng <zhuzeng@umich.edu>
2019-06-12 10:56:01 -0400

    Rename variables, add #print test and make it pass

--- mystery.rb
+++ mystery.rb
@@ -1,27 +1,40 @@
 require 'open-uri'
 file = open('http://www.gutenberg.org/files/11/11-0.txt')
 text = file.read
+
 text.gsub!(/-/, ' ')
+
 words = text.split
-cwords = []
-words.each do |w|
-  w.gsub!(/[,\?\.‘’“”\:;!\(\)]/, '')
-  cwords << w.downcase
+
+cleaned_words = []
+
+words.each do |word|
+  word.gsub!(/[,\?\.‘’“”\:;!\(\)]/, '')
+  cleaned_words << word.downcase
 end
-words = cwords
+
+words = cleaned_words
 words.sort!
-whash = {}
-words.each do |w|
-  if whash[w].nil?
-    whash[w] = 0
+
+word_hash = {}
+
+words.each do |word|
+  if word_hash[word].nil?
+    word_hash[word] = 0
   end
-  whash[w] += 1
+  word_hash[word] += 1
 end
-whash = whash.sort_by { |k, v| v }.to_h
-swords = words.sort_by do |el|
-  el.length
-end
-lword = swords.last
-whash.each do |k, v|
-  puts "#{k.ljust(lword.length + 3 - v.to_s.length, '.')}#{v}"
+
+word_hash = word_hash.sort_by { |word, count| count }.to_h
+
+MIN_NUMBER_OF_DOTS = 3
+
+def print(words, word_counts)
+  longest_word = words.max_by(&:length)
+  longest_length = longest_word.length + word_counts[longest_word].to_s.length + MIN_NUMBER_OF_DOTS
+
+  words.map do |word|
+    count = word_counts[word]
+    "#{word.ljust(longest_length - count.to_s.length + 1, '.')}#{count}"
+  end
 end
--- mystery_spec.rb
+++ mystery_spec.rb
@@ -1,5 +1,21 @@
+require 'rspec'
+require_relative './mystery'
+
 RSpec.describe 'mystery' do
-  it 'does things' do
-    # put your test code here
+  describe '#print' do
+    it 'joins the word and count with dots to equal to the longest word length plus 3 dots' do
+      word_counts = {
+        'hello' => 4,
+        'world' => 17
+      }
+
+      words = [
+        'hello',
+        'world'
+      ]
+
+      result = print(words, word_counts)
+      expect(result).to eq(["hello....4", "world...17"])
+    end
   end
 end
